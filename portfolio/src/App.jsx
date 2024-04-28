f4fe964ea97034edf7384c0a80e141d04ba8b197
Ehab Ahmed <ehabahmed2456@gmail.com>
2024-04-28 00:20:17 -0400

    adjusting projects to be displayed and styles

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,19 +1,22 @@
-import { Contact } from '../../react-portfolio/src/components/Contact/Contact'
-import { Experience } from '../../react-portfolio/src/components/Experience/Experience'
-import { Hero } from '../../react-portfolio/src/components/Hero/Hero'
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { About } from './components/About/About'
+import { Hero } from './components/Hero/Hero'
+import { Experience } from './components/Experience/Experience'
+import { Projects } from './components/Projects/Projects'
+import { Contact } from './components/Contact/Contact'
+
 function App() {
  
   return (
     <>
       <div className={styles.App}>
-        <Navbar/>
-        <Hero/>
-        <Experience/>
-        <Contact/>
-        <About/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
       </div>
     </>
   )
