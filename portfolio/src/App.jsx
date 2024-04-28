15f93dee3dbfc6843c7ae7bcf6706bc8220abbba
Ehab Ahmed <ehabahmed2456@gmail.com>
2024-04-27 23:55:13 -0400

    fixed content issue on main page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,19 @@
+import { Contact } from '../../react-portfolio/src/components/Contact/Contact'
+import { Experience } from '../../react-portfolio/src/components/Experience/Experience'
+import { Hero } from '../../react-portfolio/src/components/Hero/Hero'
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
-
+import { About } from './components/About/About'
 function App() {
  
   return (
     <>
       <div className={styles.App}>
         <Navbar/>
+        <Hero/>
+        <Experience/>
+        <Contact/>
+        <About/>
       </div>
     </>
   )
